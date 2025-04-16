8de927cf06c8862dc7f57ceedc986c198281d21d
Muhammad <muxammad_205@mail.ru>
2025-04-16 12:58:27 +0500

    feat: enhance ESLint configuration and add VSCode settings for i18n

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,21 @@ module.exports = {
     'import/order': [
       'error',
       {
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        'newlines-between': 'always',
+        pathGroups: [
+          '@processes/**',
+          '@pages/**',
+          '@widgets/**',
+          '@features/**',
+          '@entities/**',
+          '@shared/**',
+        ].map(pattern => ({
+          pattern,
+          group: 'internal',
+          position: 'after',
+        })),
+        pathGroupsExcludedImportTypes: ['builtin'],
         groups: [
           'builtin',
           'external',
@@ -44,14 +59,137 @@ module.exports = {
           'sibling',
           'index',
         ],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
       },
-
     ],
+    'import/no-restricted-paths': [
+      'error',
+      {
+        zones: [
+          // processes
+          {
+            target: 'src/processes',
+            from: 'src/app',
+          },
+          // Cross import
+          {
+            target: 'src/processes/*/**/*',
+            from: 'src/processes/*/index.ts',
+          },
+
+          // pages
+          {
+            target: 'src/pages',
+            from: 'src/app',
+          },
+          {
+            target: 'src/pages',
+            from: 'src/processes',
+          },
+          // Cross import
+          {
+            target: 'src/pages/*/**/*',
+            from: 'src/pages/*/index.ts',
+          },
+
+          // widgets
+          {
+            target: 'src/widgets',
+            from: 'src/app',
+          },
+          {
+            target: 'src/widgets',
+            from: 'src/processes',
+          },
+          {
+            target: 'src/widgets',
+            from: 'src/pages',
+          },
+          // Cross import
+          {
+            target: 'src/widgets/*/**/*',
+            from: 'src/widgets/*/index.ts',
+          },
+
+          // features
+          {
+            target: 'src/features',
+            from: 'src/app',
+          },
+          {
+            target: 'src/features',
+            from: 'src/processes',
+          },
+          {
+            target: 'src/features',
+            from: 'src/pages',
+          },
+          {
+            target: 'src/features',
+            from: 'src/widgets',
+          },
+          // Cross import
+          {
+            target: 'src/features/*/**/*',
+            from: 'src/features/*/index.ts',
+          },
+
+          // entities
+          {
+            target: 'src/entities',
+            from: 'src/app',
+          },
+          {
+            target: 'src/entities',
+            from: 'src/processes',
+          },
+          {
+            target: 'src/entities',
+            from: 'src/pages',
+          },
+          {
+            target: 'src/entities',
+            from: 'src/widgets',
+          },
+          {
+            target: 'src/entities',
+            from: 'src/features',
+          },
+          // Cross import
+          {
+            target: 'src/entities/*/**/*',
+            from: 'src/entities/*/index.ts',
+          },
+
+          // shared
+          {
+            target: 'src/shared',
+            from: 'src/app',
+          },
+          {
+            target: 'src/shared',
+            from: 'src/processes',
+          },
+          {
+            target: 'src/shared',
+            from: 'src/pages',
+          },
+          {
+            target: 'src/shared',
+            from: 'src/widgets',
+          },
+          {
+            target: 'src/shared',
+            from: 'src/features',
+          },
+          {
+            target: 'src/shared',
+            from: 'src/entities',
+          },
+        ],
+      },
+    ],
+    'import/default': 'off',
+    'import/no-named-as-default-member': 'off',
     'sort-imports': [
       'error',
       {
